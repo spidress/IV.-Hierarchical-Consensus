0b6580cea4dd46742a8a7dc7bbf7cf9273bbdd46
spidress <43718487+spidress@users.noreply.github.com>
2023-05-25 22:27:33 +0100

    Add files via upload

---
+++ puzzle/job.h
@@ -0,0 +1,46 @@
+#ifndef __JOB__
+#define __JOB__
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <set>
+#include <mutex>
+#include <thread>
+
+//#ifdef _WIN32
+//#include <windows.h>
+//#else
+//#include <unistd.h>
+//#endif
+
+#include "tx.h"
+#include "fifo.h"
+#include "md5_encode.h"
+#include "client.h"
+using namespace std;
+
+#define SLEEPTIME 2
+
+extern int g_iWorker;
+extern mutex      g_num_mutex;
+extern Fifo<int> g_TaskStack;
+extern Fifo<string> g_RecStack;
+extern Tasks g_tasks;
+extern Fifo<string> g_NotFound;
+extern Blk* g_blk;
+
+#define FIFO_SIZE 100
+
+bool startJob();
+bool newJob(int pk, int iFrom, string strDetail);
+bool queryJob(int iPK, int iFrom);
+bool repBlk(int iPK, int iFrom, string strMsg);
+bool recJob(bool bSend);
+bool recBlock(int iPK, int iFrom, string strMsg);
+bool printJob();
+void printTips(set<Tx*> t);
+void printLongTips(map<int, Tx*> t);
+#endif
---
+++ puzzle/load.cpp
@@ -0,0 +1,144 @@
+#include "load.h"
+
+bool loadWorker(string &strWorkerDetail)
+{
+	ifstream fWorker("Order.txt");
+	if(!fWorker)
+	{
+		fWorker.close();
+		printf("cannot open Order.txt\n");
+		return false;
+	}
+	getline(fWorker, strWorkerDetail);
+	//fWorker >> iWorker;
+	fWorker.close();
+	return true;
+}
+
+bool loadAllWorkers(map<int, string> &g_mWorkerDetails)
+{
+	//match IP and port with worker_id
+	string strWorkerDetail, strIP;
+	int iWorkerTmp, iPort;
+	bool bWork = false;
+	//load workers' details
+	ifstream fWorkers("Workers.txt");
+
+	if(!fWorkers)
+	{
+		fWorkers.close();
+		printf("loadAllWorkers err, cannot open Workers.txt\n");
+		return false;
+	}
+
+	while(getline(fWorkers, strWorkerDetail))
+	{
+		stringstream ss(strWorkerDetail);
+		ss >> iWorkerTmp >> strIP >> iPort >> bWork;
+		g_mWorkerDetails.insert(pair<int, string>(iWorkerTmp, strWorkerDetail));
+	}
+	fWorkers.close();
+	return true;
+}
+
+bool loadAllGPs(map<int, string> &g_mGroups)
+{
+	//match IP and port with worker_id
+	string strWorkerDetail, strIP;
+	int iWorkerTmp, iPort;
+	bool bWork = false;
+	//load workers' details
+	ifstream fGroups("Groups.txt");
+
+	if(!fGroups)
+	{
+		fGroups.close();
+		printf("cannot open Groups.txt\n");
+		return false;
+	}
+
+	while(getline(fGroups, strWorkerDetail))
+	{
+		stringstream ss(strWorkerDetail);
+		ss >> iWorkerTmp >> strIP >> iPort >> bWork;
+		g_mGroups.insert(pair<int, string>(iWorkerTmp, strWorkerDetail));
+	}
+	fGroups.close();
+	return true;
+}
+
+bool loadWorkers(map<int, string> &g_mWorkerDetails)
+{
+	//match IP and port with worker_id
+	string strWorkerDetail, strIP;
+	int iWorkerTmp, iPort;
+	bool bWork = false;
+	//load workers' details
+	ifstream fWorkers("Workers.txt");
+
+	if(!fWorkers)
+	{
+		fWorkers.close();
+		printf("loadWorkers error, cannot open Workers.txt\n");
+		return false;
+	}
+
+	while(getline(fWorkers, strWorkerDetail))
+	{
+		stringstream ss(strWorkerDetail);
+		ss >> iWorkerTmp >> strIP >> iPort >> bWork;
+		if(bWork)
+			g_mWorkerDetails.insert(pair<int, string>(iWorkerTmp, strWorkerDetail));
+	}
+	return true;
+}
+
+string loadWorkerDetail(int iWorker)
+{
+	if(!loadAllWorkers(g_mWorkerDetails)) return "";
+	if(g_mWorkerDetails.size() == 0) return "";
+	else return g_mWorkerDetails[iWorker];
+}
+
+bool loadWorkerDetail(int iWorker, string &strIP, int &iPort, bool &bWork)
+{
+	if(!loadAllWorkers(g_mWorkerDetails)) return false;
+	if(g_mWorkerDetails.size() == 0) return false;
+	if(g_mWorkerDetails.find(iWorker) == g_mWorkerDetails.end()) return false;
+	stringstream ssDetail(g_mWorkerDetails[iWorker]);
+	ssDetail >> iWorker >> strIP >> iPort >> bWork;
+	return true;
+}
+
+double loadTime()
+{
+	double process_time;
+#ifdef _WIN32
+	//DWORD now;
+	//now = GetTickCount(); //从操作系统启动经过的毫秒数
+	//cout << "Time = " << now << "ms\n ";
+	SYSTEMTIME st = { 0 };  
+	GetLocalTime(&st);  //获取当前时间 可精确到ms
+	process_time = ((st.wHour * 60 + st.wMinute) *60 + st.wSecond)*1000 + st.wMilliseconds;
+#else
+    struct timeval tv; 
+    struct timezone tz; 
+
+	/* 获取时间，理论到us */
+	gettimeofday(&tv, &tz);
+	double dSec = tv.tv_sec * 1000;
+	double dMillSec = tv.tv_usec / 1000;
+//	printf("second = %.0lf, millsecond = %.0lf\n", dSec, dMillSec);
+	process_time = dSec + dMillSec;
+	//process_time = now.tv_sec*1000 + now.tv_usec/1000;
+
+	//time_t tt;
+	//time( &tt );
+	////tt = tt + 8*3600;  // transform the time zone
+	//tm* t= gmtime( &tt );
+	////cout << tt << endl;
+	//
+	//process_time = (t->tm_hour * 60 + t->tm_min) *60 + t->tm_sec;
+#endif
+	return process_time;
+}
